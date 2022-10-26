1fc82592f009d09046db47dad9bc91a3c5834974
Chryssie Ta <development@chryssie.net>
2022-10-26 12:16:31 +1100

    Add project files.

---
+++ Portable.System.DateOnly.MinimalReplication.Dependency/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MinimalReplication.Dependency
+{
+    public class Class1
+    {
+        public static DateOnly Today => DateOnly.FromDateTime(DateTime.Now);
+    }
+}
---
+++ Portable.System.DateOnly.MinimalReplication/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MinimalReplication.Dependency;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Class1.Today);
